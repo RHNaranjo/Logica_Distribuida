ec7dff6dee7da923f834f734c480989cce89b31c
Rodrigo Naranjo <rodrinjohdez@gmail.com>
2026-09-11 13:22:10 -0600

    Archivo de salud del LB

---
+++ internal/loadbalancer/salud.go
@@ -0,0 +1,33 @@
+package loadbalancer
+
+import (
+	"net/http"
+	"time"
+)
+
+// Revisar cada instancia cada X tiempo
+func ViligarSalud(reg *Registro, cada time.Duration) {
+	cliente := &http.Client{Timeout: 2 * time.Second}
+
+	ticker := time.NewTicker(cada)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		for _, obj := range reg.objetivos() {
+			// Obtener la salud de los URLs uno por uno
+			sana := responde(cliente, obj.url)
+			reg.MarcarSalud(obj.modulo, obj.nombre, sana)
+		}
+	}
+}
+
+// Devuelve la salud de la instancia
+func responde(cliente *http.Client, url string) bool {
+	resp, err := cliente.Get(url + "/salud")
+	if err != nil {
+		return false
+	}
+	resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK
+}
